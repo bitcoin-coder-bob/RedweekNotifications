23edb1fb41fe03c21a7e08e61ec84c217f3312de
stephenfriedman <friedmansemail@gmail.com>
2018-02-13 12:47:19 -0500

    txt me and bro, no text file, deploying to heroku, no old code

--- redweek.js
+++ redweek.js
@@ -13,15 +13,11 @@ const ENV_FROM = process.env.from;
 const ENV_TO = process.env.to;
 const ENV_TO2 = process.env.to2;
 const ENV_APP_URL = process.env.appURL;
+let redweekUrl = 'https://www.redweek.com/resort/P4872-marriotts-aruba-surf-club/rentals?sort-rentals=newest&amp;type=rentals&amp;sort=newest';
 
-
-
+const client = require('twilio')(ENV_ACCOUNT_SID, ENV_AUTH_TOKEN);
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var unique = require('array-unique');
-//var read = require('read-file');
-//const writeFile = require('write-file')
-
-let redweekUrl = 'https://www.redweek.com/resort/P4872-marriotts-aruba-surf-club/rentals?sort-rentals=newest&amp;type=rentals&amp;sort=newest';
 var express = require('express');
 var server = express();
 var server_port = ENV_PORT  || 80;
@@ -30,7 +26,6 @@ server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
-const client = require('twilio')(ENV_ACCOUNT_SID, ENV_AUTH_TOKEN);
 
 function makeHttpObject() {
   try {return new XMLHttpRequest();}
@@ -47,22 +42,7 @@ function makeHttpObject() {
 function filterResults(res) {
   let filtered = new Array(res.length)
   filtered = unique(res);
-  // read('postings.txt', 'utf8', function(err, buffer) {
-  //   comparePostings(buffer,filtered);  
-  // });
   comparePostings(globalPostings,filtered);
-  //stringify newest (unique) postings
-  // var rawFilteredPostingsString = "";
-  // filtered.forEach(function(f){
-  //   rawFilteredPostingsString=rawFilteredPostingsString.concat(f+"\r\n");
-  // })
-  
-  //overwrite most recent postings to textfile
-  //console.log("Newest postings: \r\n",rawFilteredPostingsString)
-  // writeFile('postings.txt', rawFilteredPostingsString, function (err) {
-  //   if (err) return console.log(err)
-  //   console.log("Overwrote file")
-  // })
 }
 
 //determines if there are new postings and sends text
@@ -70,6 +50,7 @@ function comparePostings(storedPosts, newPosts) {
   let textMessage = "";
   newPosts.forEach(function(newPost) {
    if(!storedPosts.includes(newPost)){
+    globalPostings.push(newPost);
     textMessage=textMessage.concat("https://www.redweek.com"+newPost+" \r\n")
    }
   });
@@ -86,21 +67,20 @@ function comparePostings(storedPosts, newPosts) {
         process.stdout.write(message.sid);
       }
     );
-    // client.messages.create(
-    //   {
-    //     body: textMessage,
-    //     to: ENV_TO2,
-    //     from: ENV_FROM
-    //   },
-    //   (err, message) => {
-    //     process.stdout.write(message.sid);
-    //   }
-    // );
+    client.messages.create(
+      {
+        body: textMessage,
+        to: ENV_TO2,
+        from: ENV_FROM
+      },
+      (err, message) => {
+        process.stdout.write(message.sid);
+      }
+    );
   }
   else{
     console.log("no new listings, no text message sent");
   }
-  globalPostings = newPosts;
 }
 
 function runner(){
@@ -131,20 +111,12 @@ request.onreadystatechange = function() {
   {
     let rawPostings = request.responseText.match(regex);
     let res = rawPostings;
-    let filtered = new Array(res.length)
-    filtered = unique(res);    
-    globalPostings=filtered;
-    //stringify newest (unique) postings
-    // var rawFilteredPostingsString = "";
-    // filtered.forEach(function(f){
-    //   rawFilteredPostingsString=rawFilteredPostingsString.concat(f+"\r\n");
-    // })
-    //overwrite most recent postings to textfile
+    let filtered = [];//new Array(res.length)
+    filtered = unique(res);
+    filtered.forEach(function(f){
+      globalPostings.push(f);
+    }) 
     console.log("GLOBAL_POSTINGS: \r\n",globalPostings)
-    // writeFile('postings.txt', rawFilteredPostingsString, function (err) {
-    //   console.log("Populating postings.txt")      
-    //   if (err) return console.log(err)
-    // });
   }
 };
 
